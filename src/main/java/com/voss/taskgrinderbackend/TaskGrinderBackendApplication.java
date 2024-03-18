f09f3fdc4b5abc5de61c9938e7cb761a6eed41d2
Folke <vossfolke@gmail.com>
2024-03-18 14:51:06 +0100

    Initial Commit

---
+++ src/main/java/com/voss/taskgrinderbackend/TaskGrinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.voss.taskgrinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskGrinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskGrinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voss/taskgrinderbackend/TaskGrinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voss.taskgrinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskGrinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
