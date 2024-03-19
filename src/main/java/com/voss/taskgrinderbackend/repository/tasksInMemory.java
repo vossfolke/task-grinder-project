c92d7dd330096bfb683589c317e3470f295846dd
vossfolke <129680876+vossfolke@users.noreply.github.com>
2024-03-19 08:50:26 +0100

    Added an in memory data storage (List)

--- src/main/java/com/voss/taskgrinderbackend/repository/tasksInMemory.java
+++ src/main/java/com/voss/taskgrinderbackend/repository/tasksInMemory.java
@@ -1,1 +1,12 @@
+package com.voss.taskgrinderbackend.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.voss.taskgrinderbackend.service.Task;
+
+public class tasksInMemory{
+
+    private List<Task> tasks = new ArrayList<>();
+
+}
