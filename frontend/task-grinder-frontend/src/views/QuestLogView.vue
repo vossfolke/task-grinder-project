8f4e7617c51b26620dd3b3d49a8e529fe09a31be
Folke <vossfolke@gmail.com>
2024-03-24 20:27:25 +0100

    changed task creation menu. dropdown list when weekly

--- frontend/task-grinder-frontend/src/views/QuestLogView.vue
+++ frontend/task-grinder-frontend/src/views/QuestLogView.vue
@@ -4,29 +4,31 @@
       <a href="/questlog" class="nav__link" >Quest Hub</a>
     </nav>
   <div class="about">
-    <p>With this web app you can manage your tasks and give them a quest like character. You can assign a 
-      difficulty to your tasks based on the motivation you need to complete them. Earn xp by completing tasks in time to level up. 
-      As a motivation you can define rewards for leveling up. 
-      A reward may be a nice Diner or watching a movie in cinema. This should help you building a routine doing daily tasks that feels rewarding.
-    </p>
     <form v-on:submit.prevent="submitTask">
           <select v-model="questType" id="questTimeChoice">
             <option disabled value="">Select Quest Type</option>
             <option v-for="option in questOptions" :value="option">{{ option }}</option>
           </select>
       <input type="text" placeholder="task">
-      <input type="date" placeholder="today">
+      <select v-if="questType === 'weekly'" id="selectWeekly" >
+        <option disabled value="">Select Day</option>
+        <option v-for="day in weekdays" :value="day">{{ day }}</option>
+      </select>
+      <input  v-else type="date" placeholder="today">
       <button>Commit</button>
     </form>
   </div>
 </template>
-
 <script>
+
+
 export default {
   data() {
     return {
-      questType: 'Quest Type',
-      questOptions: ['once', 'weekly', 'monthly']
+      questType: '',
+      questOptions: ['once', 'weekly', 'monthly'],
+      selectedWeekday: '',
+      weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     }
   },
   methods: {
