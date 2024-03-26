53d0fcb78d431aecbbddcc262b6e0e34d1346755
Folke Voss <vossfolke@gmail.com>
2024-03-26 11:17:17 +0000

    added foreign key (profile) to task

--- backend/src/main/java/com/voss/taskgrinderbackend/Service/TaskServiceImpl.java
+++ backend/src/main/java/com/voss/taskgrinderbackend/Service/TaskServiceImpl.java
@@ -1,14 +1,11 @@
 package com.voss.taskgrinderbackend.Service;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.voss.taskgrinderbackend.repository.TaskRepository;
-import com.voss.taskgrinderbackend.repository.TasksInMemory;
 import com.voss.taskgrinderbackend.pojo.Task;
 
 @Service
--- backend/src/main/java/com/voss/taskgrinderbackend/pojo/Task.java
+++ backend/src/main/java/com/voss/taskgrinderbackend/pojo/Task.java
@@ -1,12 +1,13 @@
 package com.voss.taskgrinderbackend.pojo;
 
-import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.util.Date;
@@ -24,11 +25,15 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(name = "quest_name", nullable = false)
     private String name;
-    @Column
+    @Column(name = "date", nullable = false)
     private Date date;
-    @Column(name = "questType")
+    @Column(name = "questType", nullable = false)
     private String value;
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "userID", name = "profile_id")
+    private Profile profile;
 
 }
